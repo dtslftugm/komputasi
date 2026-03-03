809d3931ee1ac04253fa6cefd9f0732c6460bc67
dtslftugm <agus.santoso.jtsl@ugm.ac.id>
2026-03-03 15:05:51 +0700

    Update API_URL for live deployment
    Updated API_URL to point to the live deployment URL and commented out the beta tester URL.

--- js/config.js
+++ js/config.js
@@ -3,9 +3,11 @@
  * Update API_URL with your actual Google Apps Script deployment URL
  */
 var CONFIG = {
-  // TODO: Replace with your actual GAS deployment URL after deployment
-  API_URL: 'https://script.google.com/macros/s/AKfycbxCgq1JLHx3gfVcYVXCpZ3xel5Sfv6vTldJBQG8qP6Xx-XLLMihaGE1Uf4hE7Y7mYXF/exec',
-
+  // URL Beta Tester
+//  API_URL: 'https://script.google.com/macros/s/AKfycbxCgq1JLHx3gfVcYVXCpZ3xel5Sfv6vTldJBQG8qP6Xx-XLLMihaGE1Uf4hE7Y7mYXF/exec',
+  
+  //URL live
+  API_URL: 'https://script.google.com/macros/s/AKfycbyPCAs7bebTWcXkWsJ9Qi0paL2o88HT-4z5c-bTNVsQ73n4t_2mTndXsGC7dYIXIyIN/exec',
   APP_NAME: 'Layanan Komputasi DTSL FT UGM',
   APP_VERSION: '17.2',
   ENVIRONMENT: 'production'
@@ -16,3 +18,4 @@ window.CONFIG = CONFIG;
 if (typeof module !== 'undefined' && module.exports) {
   module.exports = CONFIG;
 }
+
