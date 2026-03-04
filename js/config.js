09b26e893ebb622d953f367b2c6c6b316cabe380
dtslftugm <agus.santoso.jtsl@ugm.ac.id>
2026-03-04 09:53:04 +0700

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -4,8 +4,8 @@
  */
 var CONFIG = {
   // URL Beta Tester
-//  API_URL: 'https://script.google.com/macros/s/AKfycbxCgq1JLHx3gfVcYVXCpZ3xel5Sfv6vTldJBQG8qP6Xx-XLLMihaGE1Uf4hE7Y7mYXF/exec',
-  
+  //  API_URL: 'https://script.google.com/macros/s/AKfycbxCgq1JLHx3gfVcYVXCpZ3xel5Sfv6vTldJBQG8qP6Xx-XLLMihaGE1Uf4hE7Y7mYXF/exec',
+
   //URL live
   API_URL: 'https://script.google.com/macros/s/AKfycbyPCAs7bebTWcXkWsJ9Qi0paL2o88HT-4z5c-bTNVsQ73n4t_2mTndXsGC7dYIXIyIN/exec',
   APP_NAME: 'Layanan Komputasi DTSL FT UGM',
@@ -18,4 +18,3 @@ window.CONFIG = CONFIG;
 if (typeof module !== 'undefined' && module.exports) {
   module.exports = CONFIG;
 }
-
